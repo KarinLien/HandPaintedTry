07802fec0c3ca8cd3e4033a848f0cf25934f3f2d
karin <karin.lien.c.l@emphasis.com.tw>
2019-06-06 16:22:17 +0800

    sec update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-
+//https://medium.com/the-andela-way/how-to-deploy-your-react-application-to-github-pages-in-less-than-5-minutes-8c5f665a2d2a
 function App() {
   return (
     <div className="main-app">
