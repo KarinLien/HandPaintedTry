4013a184e6188d2612ee0b4dd03a476936ddc035
karin <karin.lien.c.l@emphasis.com.tw>
2019-06-11 14:07:32 +0800

    update 0611

--- src/components/LeftMenu/index.js
+++ src/components/LeftMenu/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./LeftMenu.css";
+import img from "../../assets/abc.jpg";
 
 const data = ["1", "2", "3"];
 
 class LeftMenu extends React.Component {
   render() {
+    const { menuItem } = this.props;
     return (
       <div className="left-div">
         <ul className="menu-outside-border">
@@ -14,7 +16,10 @@ class LeftMenu extends React.Component {
               onClick={() => this.props.clickMenuItem(item)}
               className="menu-item-style"
               style={{
-                background: item === this.props.menuItem ? "pink" : ""
+                background:
+                  item === menuItem
+                    ? `url(${img})  no-repeat center center`
+                    : ""
               }}
             >
               {item}
--- src/components/RightContent/index.js
+++ src/components/RightContent/index.js
@@ -7,19 +7,17 @@ const Content = props => {
     case "1":
       return (
         <wired-card elevation="1" style={{ width: "60%" }}>
-          <p>Elevation: 1</p>
+          <p>title: 1</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            <mark>Ut enim ad minim veniam</mark>, quis nostrud exercitation
-            ullamco laboris nisi ut aliquip ex ea commodo consequat.
           </p>
         </wired-card>
       );
     case "2":
       return (
         <wired-card elevation="2" style={{ width: "60%" }}>
-          <p>Elevation: 2</p>
+          <p>title: 2</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elitlamco.
             laboris nisi ut aliquip ex ea commodo consequat.
@@ -29,7 +27,7 @@ const Content = props => {
     case "3":
       return (
         <wired-card elevation="3" style={{ width: "60%" }}>
-          <p>Elevation: 3</p>
+          <p>title: 3</p>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -50,7 +48,6 @@ class RightContent extends React.Component {
     const { menuItem } = this.props;
     return (
       <div className="right-div">
-        <p>RightContent</p>
         <Content menuItem={menuItem ? menuItem : undefined} />
       </div>
     );
